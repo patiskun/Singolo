27ec94721eea89a1ec06aa054e70beab7cf1380b
Pavel <pashutka.koltusha@gmail.com>
2022-03-01 20:02:36 +0300

    add: all except footer and media

--- js/slider.js
+++ js/slider.js
@@ -7,20 +7,37 @@ const nextBtn = document.querySelector('#nextBtn');
 let counter = 1;
 const size = sliderImages[0].clientWidth;
 
+prevBtn.src="img/Rarrow-left.svg";
+nextBtn.src="img/Rarrow-right.svg";
+
 sliderContent.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click',()=>{
     if(counter >= sliderImages.length-1) return;
-    sliderContent.style.transition = "transform 0.4s ease-in-out";
+    sliderContent.style.transition = "transform 0.1s ease-in-out";
     counter++;
     sliderContent.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    if(counter % 2 == 0){
+        prevBtn.src="img/arrow-left.svg";
+        nextBtn.src="img/arrow-right.svg";
+    } else {
+        prevBtn.src="img/Rarrow-left.svg";
+        nextBtn.src="img/Rarrow-right.svg";
+    }
 });
 
 prevBtn.addEventListener('click',()=>{
     if(counter<=0) return;
-    sliderContent.style.transition = "transform 0.4s ease-in-out";
+    sliderContent.style.transition = "transform 0.1s ease-in-out";
     counter--;
     sliderContent.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    if(counter % 2 == 0){
+        prevBtn.src="img/arrow-left.svg";
+        nextBtn.src="img/arrow-right.svg";
+    } else {
+        prevBtn.src="img/Rarrow-left.svg";
+        nextBtn.src="img/Rarrow-right.svg";
+    }
 });
 
 sliderContent.addEventListener('transitionend', ()=>{
